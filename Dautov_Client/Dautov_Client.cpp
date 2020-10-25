7b7bbacb5de8864c172f7a13de3f0687ace37d6a
ildar58 <ildar.dautov01@gmail.com>
2020-10-25 22:59:35 +0300

    initial commit

---
+++ Dautov_Client/Dautov_Client.cpp
@@ -0,0 +1,130 @@
+﻿// Dautov_Client.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include "Dautov_Client.h"
+#include "Msg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+unsigned int connection = 1;
+mutex hMutex;
+
+enum Answers {
+    MESSAGE = 1,
+    EXIT    = 2
+};
+
+void listenServer() {
+    Message m;
+    while (connection) {
+        m = Message::Send(M_BROKER, M_GETDATA);
+        if (m.m_Header.m_Type != M_NODATA)
+        {
+            hMutex.lock();
+            cout << "===============================================" << endl;;
+            cout << "Message from client - " << m.m_Header.m_From << ":" << endl;
+            cout << m.m_Data << endl;
+            cout << "===============================================" << endl;
+            hMutex.unlock();
+        }
+
+        Sleep(2000);
+    }
+}
+
+void Connect()
+{
+    Message::Send(M_BROKER, M_INIT);
+    hMutex.lock();
+    cout << "Your ID is " << Message::m_ClientID << endl;
+    hMutex.unlock();
+    thread t(listenServer);
+    t.detach();
+}
+
+void Process() {
+    Message m;
+    unsigned int answer;
+
+    Connect();
+    
+    while (true)
+    {
+        hMutex.lock();
+        cout << "1. Send message \n2. Exit" << endl;
+        hMutex.unlock();
+        cin >> answer;
+
+        switch (answer)
+        {
+        case MESSAGE: 
+        {
+            hMutex.lock();
+            cout << "Enter ID of client: ";
+            cin >> m.m_Header.m_To;
+
+            cout << "Enter your message: ";
+            cin >> m.m_Data;
+
+            Message::Send(m.m_Header.m_To, M_DATA, m.m_Data);
+            cout << "Message was send\n\n";
+            hMutex.unlock();
+            break;
+        }
+        case EXIT:
+        {
+            Message::Send(M_BROKER, M_EXIT);
+            hMutex.lock();
+            cout << "Session ended\n\n" << endl;
+            hMutex.unlock();
+            return;
+        }
+        default:
+            hMutex.lock();
+            cout << "Press 0 or 1\n\n" << endl;
+            hMutex.unlock();
+        }
+    }
+}
+
+
+// Единственный объект приложения
+
+CWinApp theApp;
+
+using namespace std;
+
+int main()
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+
+    if (hModule != nullptr)
+    {
+        // инициализировать MFC, а также печать и сообщения об ошибках про сбое
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            // TODO: вставьте сюда код для приложения.
+            wprintf(L"Критическая ошибка: сбой при инициализации MFC\n");
+            nRetCode = 1;
+        }
+        else
+        {
+            AfxSocketInit();
+            Process();
+            // TODO: вставьте сюда код для приложения.
+        }
+    }
+    else
+    {
+        // TODO: измените код ошибки в соответствии с потребностями
+        wprintf(L"Критическая ошибка: сбой GetModuleHandle\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
---
+++ Dautov_Server/Dautov_Server.cpp
@@ -0,0 +1,127 @@
+﻿// Dautov_Server.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include "Dautov_Server.h"
+#include "Msg.h"
+#include "Session.h"
+
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+int gMaxID = M_USER;
+map<int, shared_ptr<Session>> gSessions;
+
+void ProcessClient(SOCKET hSock)
+{
+    CSocket s;
+    s.Attach(hSock);
+    Message m;
+    
+    int nCode = m.Receive(s);
+    if (nCode != M_GETDATA)
+        cout << m.m_Header.m_From << ": " << nCode << endl;
+    switch (nCode)
+    {
+        case M_INIT:
+        {
+            auto pSession = make_shared<Session>(++gMaxID, m.m_Data);
+            gSessions[pSession->m_ID] = pSession;
+            Message::Send(s, pSession->m_ID, M_BROKER, M_INIT);
+            break;
+        }
+        case M_EXIT:
+        {
+            gSessions.erase(m.m_Header.m_From);
+            Message::Send(s, m.m_Header.m_From, M_BROKER, M_CONFIRM);
+            return;
+        }
+        case M_GETDATA:
+        {
+            if (gSessions.find(m.m_Header.m_From) != gSessions.end())
+            {
+                gSessions[m.m_Header.m_From]->Send(s);
+            }
+            break;
+        }
+        default:
+        {
+            if (gSessions.find(m.m_Header.m_From) != gSessions.end())
+            {
+                if (gSessions.find(m.m_Header.m_To) != gSessions.end())
+                {
+                    gSessions[m.m_Header.m_To]->Add(m);
+                }
+                else if (m.m_Header.m_To == M_ALL)
+                {
+                    for (auto& [id, Session] : gSessions)
+                    {
+                        if (id != m.m_Header.m_From)
+                            Session->Add(m);
+                    }
+                }
+                Message::Send(s, m.m_Header.m_From, M_BROKER, M_CONFIRM);
+            }
+            break;
+    }   
+    }
+}
+
+
+void Server()
+{
+    AfxSocketInit();
+
+    CSocket Server;
+    Server.Create(12345);
+
+    while (true)
+    {
+        if (!Server.Listen())
+            break;
+        CSocket s;
+        Server.Accept(s);
+        thread t(ProcessClient, s.Detach());
+        t.detach();
+    }
+}
+
+
+
+// Единственный объект приложения
+
+CWinApp theApp;
+
+using namespace std;
+
+int main()
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+
+    if (hModule != nullptr)
+    {
+        // инициализировать MFC, а также печать и сообщения об ошибках про сбое
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            // TODO: вставьте сюда код для приложения.
+            wprintf(L"Критическая ошибка: сбой при инициализации MFC\n");
+            nRetCode = 1;
+        }
+        else
+        {
+            Server();
+        }
+    }
+    else
+    {
+        // TODO: измените код ошибки в соответствии с потребностями
+        wprintf(L"Критическая ошибка: сбой GetModuleHandle\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
